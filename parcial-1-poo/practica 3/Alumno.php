99275acaf521ba36ac69b36b9296970d19d66728
AlonsoCovarrubias17 <alonso_covarrubias86@hotmail.com>
2026-03-03 12:44:43 -0700

    Se agrega clase Alumno.php

---
+++ parcial-1-poo/practica 3/Alumno.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/Usuario.php';
+
+class Alumno extends Usuario {
+
+    private string $matricula;
+
+    public function __construct(string $nombre, string $correo, string $matricula) {
+        parent::__construct($nombre, $correo);
+        $this->matricula = $matricula;
+    }
+
+    public function getMatricula(): string {
+        return $this->matricula;
+    }
+
+    public function getRol(): string {
+        return "Alumno";
+    }
+}
